6f8f7bf14dc5b600e7f56a9480d8832c4e156daa
egoloprum <bayraa093@gmail.com>
2026-01-28 23:07:29 +0300

    refactor: HTTP writer

--- metro/http/http_writer.h
+++ metro/http/http_writer.h
@@ -3,63 +3,68 @@
 #include <sstream>
 #include <unistd.h>
 
-#include "../context.h"
-#include "../helpers.h"
-#include "../constants.h"
+#include "context.h"
+#include "helpers.h"
+#include "constants.h"
 
 namespace Metro {
   class HttpWriter {
-  public:
-      static void write(int clientSocket, const Context& context, bool keepAlive) {
-          std::string response = buildResponse(context, keepAlive);
-          send(clientSocket, response.data(), response.size(), 0);
-      }
+    public:
+
+    static void write(int clientSocket, const Context& context, bool keepAlive) {
+      std::string response = buildResponse(context, keepAlive);
+      send(clientSocket, response.data(), response.size(), 0);
+    }
   
-  private:
-      static std::string buildResponse(const Context& context, bool keepAlive) {
-          std::ostringstream responseStream;
-          
-          writeStatusLine(responseStream, context);
-          writeHeaders(responseStream, context);
-          writeFixedHeaders(responseStream, context, keepAlive);
-          
-          // Empty line to separate headers from body
-          responseStream << "\r\n";
-          
-          // Response body
-          if (!context.res._body.empty()) {
-              responseStream << context.res._body;
-          }
-          
-          return responseStream.str();
-      }
+    private:
+
+    static std::string buildResponse(const Context& context, bool keepAlive) {
+      std::ostringstream output;
+        
+      writeStatusLine(output, context);
+      writeHeaders(output, context);
+      writeFixedHeaders(output, context, keepAlive);
+        
+      output << "\r\n";
+        
+      if (!context.res._body.empty()) { output << context.res._body; }
+        
+      return output.str();
+    }
   
-      static void writeStatusLine(std::ostream& stream, const Context& context) {
-          stream << "HTTP/1.1" << " "
-                 << context.res._status << " "
-                 << Helpers::reasonPhrase(context.res._status)
-                 << "\r\n";
-      }
+    static void writeStatusLine(std::ostream& output, const Context& context) {
+      output  << "HTTP/1.1" << " "
+              << context.res._status << " "
+              << Helpers::reasonPhrase(context.res._status)
+              << "\r\n";
+    }
   
-      static void writeHeaders(std::ostream& stream, const Context& context) {
-          for (const auto& [headerName, headerValue] : context.res._headers) {
-              stream << headerName << ": " << headerValue << "\r\n";
-          }
+    static void writeHeaders(std::ostream& output, const Context& context) {
+      for (const auto& [headerName, headerValue] : context.res._headers) {
+        output  << headerName << ": " 
+                << headerValue 
+                << "\r\n";
       }
+    }
   
-      static void writeFixedHeaders(std::ostream& stream, const Context& context, bool keepAlive) {
-          stream << Constants::Http_Header::CONTENT_LENGTH << ": " 
-                 << context.res._body.size() 
-                 << "\r\n";
-          
-          stream << Constants::Http_Header::CONNECTION << ": "
-                 << (keepAlive ? Constants::Http_Connection::KEEP_ALIVE 
-                               : Constants::Http_Connection::CLOSE)
-                 << "\r\n";
-          
-          stream << Constants::Http_Header::DATE << ": "
-                 << Helpers::getCurrentDate()
-                 << "\r\n";
+    static void writeFixedHeaders(std::ostream& output, const Context& context, bool keepAlive) {
+      auto it = context.res._headers.find(std::string(Constants::Http_Header::TRANSFER_ENCODING));
+      bool is_chunked = (it != context.res._headers.end() && it->second.find("chunked") != std::string::npos);
+
+      if (!is_chunked) {
+        output  << Constants::Http_Header::CONTENT_LENGTH << ": " 
+                << context.res._body.size() 
+                << "\r\n";
       }
+      
+      output  << Constants::Http_Header::CONNECTION << ": "
+              << (keepAlive ? Constants::Http_Connection::KEEP_ALIVE 
+                            : Constants::Http_Connection::CLOSE)
+              << "\r\n";
+      
+      output  << Constants::Http_Header::DATE << ": "
+              << Helpers::getCurrentDate()
+              << "\r\n";
+    }
   };
 }
